7373bb898f4a0e587e848bab8bae090e7053571d
Christoffer Olsen <christofferolsen22@gmail.com>
2018-05-23 10:43:17 +0200

    persondata blade og scss

---
+++ resources/views/persondata.blade.php
@@ -0,0 +1,90 @@
+@extends('layouts.master')
+
+@section('meta-desc-content', 'Frisørne Tina Maltby og Tinne Holmen byder velkommen til deres salon. Telefon: 49132356 Adresse: Skovparken 10 Espergærde')
+
+@section('content')
+		<section class="persondata_content">
+		
+		<h1> OPLYSNINGER OM PERSONDATA TIL KUNDER </h1>
+
+		<p>Retningslinjer for behandling af personoplysninger for kunder i forbindelse med opfyldelse af aftale hos Holmen og Maltby CVR-nr. 32201636</p>
+
+		<p>Som følge af din aftale med salonen/klinikken accepterer du denne privatlivspolitik. </p>
+
+		<ul>Oversigt: <br>
+		<li>1. Generelt </li>
+		<li>2. Personoplysninger </li>
+		<li>2.1 Hvad er personoplysninger? </li>
+		<li>2.2 Hvor længe opbevarer vi personoplysninger? </li>
+		<li>2.3 Hvad bruger vi personoplysninger til? </li>
+		<li>2.4 Hvordan beskytter vi dine personoplysninger? </li>
+		<li>2.5 Retten til adgang til dine personoplysninger </li>
+		<li>2.6 Retten til berigtigelse af dine personoplysninger </li>
+		<li>2.7 Retten til sletning af dine personoplysninger </li>
+		<li>2.8 Kan vi ændre behandlingen af personoplysninger? </li>
+		<li>3. Kontakt </li>
+		</ul>
+
+		<h2>1. Generelt </h2>
+		<p>Gennem din aftale med salonen/klinikken indsamles der personoplysninger om dig. 
+		Disse retningslinjer er udarbejdet ud fra EU’s Databeskyttelsesforordning, som får virkning i Danmark fra 25. maj 2018, samt den nye danske databeskyttelseslov, der supplerer reglerne i databeskyttelsesforordningen og den nuværende persondatalov (samlet ”gældende lovgivning”). 
+		Nedenfor kan du læse om de informationer, der indsamles, herunder hvordan oplysningerne behandles, hvad de bruges til, hvem der har adgang til oplysningerne, og hvem du kan kontakte, hvis du har spørgsmål eller indsigelser vedrørende de indsamlede oplysninger. </p>
+
+		<h2>2. Personoplysninger </h2>
+		<h3>2.1 Hvad er personoplysninger? </h3>
+		<p>Salonen/klinikken beder om dine almindelige kontaktoplysninger, når du bestiller tid til behandling. Det er primært oplysninger som navn, adresse, postnummer, telefon- og mobiltelefonnummer, e-mail mv. De personoplysninger, som vi indsamler, bruges til at identificere dig som kunde samt levere de services, du har købt af os. De personoplysninger, som vi indsamler i forbindelse med opfyldelsen af aftalen, vil blive opbevaret i vores IT-system og/eller i et fysisk register. </p>
+
+		<h3>2.2 Hvor længe opbevarer vi personoplysninger? </h3>
+		<p>Opbevaring af personoplysninger afhænger af, hvor længe din aftale med salonen/klinikken består. 
+		Når du køber en service hos os, har vi brug for dine personoplysninger som dokumentation på dine aftaler og behandlinger. 
+		Under det løbende aftaleforhold opbevares de oplysninger, vi har indsamlet på dig i vores IT-system, hvorefter personoplysningerne arkiveres. 
+		Dine personoplysninger vil som udgangspunkt efter gældende regler blive opbevaret i salonen/klinikken. Ved ophør som kunde i salonen/klinikken vil dine personoplysninger blive opbevaret i en periode på 5 år fra udgangen af det regnskabsår, materialet vedrører. Hvis aftalen med dig f.eks. er ophørt november 2017 og regnskabsåret følger kalenderåret, er vi efter gældende regler forpligtet til at opbevare oplysningerne indtil tidligst udgangen af 2022. Når personoplysningerne ikke længere er relevante, og vi ikke er forpligtet til at opbevare dem, vil alle personoplysninger vedrørende dig blive slettet. 
+		2.3 Hvad bruger vi personoplysninger til? 
+		De personoplysninger, vi indsamler om dig i forbindelse med opfyldelse af vores aftale, bruges først og fremmest til almindelig kundeadministration, herunder eksempelvis til opfyldelse af den aftale, du har indgået med salonen/klinikken. 
+
+		Oplysningerne vil som udgangspunkt ikke blive videregivet til tredjepart. 
+		Derudover kan vi behandle dine oplysninger, hvis behandlingen er nødvendig for, at salonen/klinikken (eller tredjemand) kan forfølge en legitim interesse, medmindre hensynet til dig går forud herfor. Anden videregivelse tillades kun, såfremt gældende lovgivning undtagelsesvis tillader videregivelse af personoplysninger. </p>
+
+		<h3>2.4 Retten til indsigt i dine personoplysninger </h3>
+		<p>Du har som kunde hos salonen/klinikken ret til at få salonen/klinikkens bekræftelse på, om personoplysninger vedrørende dig selv behandles og i givet fald adgang til personoplysningerne. 
+		Du er tillige berettiget til at anmode om og modtage personoplysninger om dig selv, som du har afgivet til salonen/klinikken. Du er desuden berettiget til at gøre indsigelse mod behandling af dine personoplysninger. </p>
+
+		<h3>2.5 Retten til berigtigelse af dine personoplysninger </h3>
+		<p>Du har ret til at få urigtige personoplysninger om dig selv berigtiget af salonen/klinikken uden unødig forsinkelse. </p>
+
+		<h3>2.6 Retten til sletning af dine personoplysninger </h3>
+		<p>Du har ret til at få personoplysninger om dig selv slettet af salonen/klinikken uden unødig forsinkelse, hvis salonen/klinikken ikke fortsat har en legitim interesse i at opbevare dine personoplysninger. 
+		Bemærk venligst, at såfremt salonen/klinikken i henhold til gældende lovgivning er forpligtet til at opbevare oplysningerne, kan du ikke kræve, at dine personoplysninger slettes. </p>
+
+		<h3>2.7 Beskyttelse af personoplysninger </h3>
+		<p>Ifølge gældende lovgivning skal dine personlige oplysninger opbevares sikkert og fortroligt. Vi gemmer de personlige oplysninger, du afgiver, i salonen/klinikkens IT-system eller fysisk i aflåst skab. 
+
+		Vores sikkerhedsforanstaltninger kontrolleres løbende for at afgøre, om vores behandling af personoplysninger håndteres forsvarligt og under hensyntagen til dine rettigheder som kunde. </p>
+
+		<h3>2.8 Ændringer i behandlingen af personoplysninger </h3>
+		<p>Vi forbeholder os ret til at opdatere og ændre nærværende retningslinjer for behandling af personoplysninger. Ændres retningslinjerne, rettes datoen for ”sidst opdateret” nederst på siden. Disse retningslinjer bliver evalueret og om nødvendigt revideret en gang årlig. </p>
+
+		<h2>3. Kontakt </h2>
+		<p>Hvis du ønsker adgang til de oplysninger, som er registreret om dig hos salonen/klinikken, skal du rette henvendelse til [Indsæt navn] på e-mail:  [Indsæt din e-mail]. 
+
+		Er der registreret forkerte data, eller har du andre indsigelser, skal du rette henvendelse samme sted. Du har mulighed for at få indsigt i, hvilke informationer der er registreret om dig. 
+
+		Hvis du ønsker at klage over salonen/klinikkens behandling af dine personoplysninger kan dette ske til 
+		Datatilsynet: </p> <br>
+
+		
+		<p>Datatilsynet <br>
+		Borgergade 28, 5 <br>
+		1300 København K <br>
+		Telefon: 33 19 32 00 <br>
+		E-mail: dt@datatilsynet.dk <br>
+		Med venlig hilsen <br>
+
+		Salon/klinik: Holmen og Maltby <br>
+		Senest revideret: 07052018 <br>
+		Næste revision: 07052019 <br>
+		</p>
+		
+		
+		</section>
+@endsection
